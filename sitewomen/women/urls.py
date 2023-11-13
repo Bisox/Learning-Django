7dd667b2a3c81b3b5cb664ecd2a36cdbbc8b254c
Evgen Bratenkov <giertrut@mail.ru>
2023-11-14 02:34:19 +0300

    add&learn_converters

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -1,11 +1,15 @@
 
-from django.urls import path
+from django.urls import path, register_converter
 from women import views
+from . import converters
 
+register_converter(converters.FourDigitYearConverter, 'year4')
 
 
 urlpatterns = [
     
     path('', views.index),
-    path('cat/', views.categories)
+    path('cats/<slug:cat_slug>/', views.categories_by_slug),
+    path('cats/<int:cat_id>/', views.categories),
+    path('archive/<year4:year>/', views.archive)
 ]
